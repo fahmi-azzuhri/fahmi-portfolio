b1ede92ef2c99bcdd8cc0434df7ea18d8f078d8c
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-08-15 22:32:11 +0700

    add vue

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { DiJavascript1, DiReact, DiGit } from "react-icons/di";
-import { SiNextdotjs } from "react-icons/si";
+import { SiNextdotjs, SiVuedotjs } from "react-icons/si";
 
 function Techstack() {
   return (
@@ -9,12 +9,14 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVuedotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
