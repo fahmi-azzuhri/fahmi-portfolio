e2842a7ff80b61c4b13a29707a129383b542c166
Fahmi Azzuhri Efki <fahmiazzuhriefki@gmail.com>
2025-06-18 23:17:32 +0700

    refactor : change ux home

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { getConfigData } from "../data/configReader";
 import { useNavigate } from "react-router-dom";
+import ProjectImages from "./ProjectImages";
 
 export default function Card() {
   const configData = getConfigData();
@@ -61,7 +62,6 @@ export default function Card() {
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}
               className="drop-shadow-md card bg-white rounded-lg px-5 py-3 gap-x-3 flex flex-col md:flex-none md:flex-row hover:-translate-y-1 hover:scale-100 duration-300 transition ease-in-out delay-150 hover:shadow-sm border border-gray-200 hover:border-gray-300 mb-2"
-              href={project["project-url"]}
             >
               <div className="flex flex-col justify-center">
                 <h1 className="font-medium text-lg">
@@ -70,23 +70,11 @@ export default function Card() {
                 <p className="text-gray-500 text-md">
                   {project["project-desc"]}
                 </p>
+                <p className="text-blue-500 text-md py-3">
+                  {project["project-url"]}
+                </p>
+                <ProjectImages images={project["project-images"]} />
               </div>
-              <button className="ml-auto hidden md:inline-block">
-                <svg
-                  className={svgClass}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                  />
-                </svg>
-              </button>
             </a>
           ))}
         </div>
